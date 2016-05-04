bb75c7acac55887a7980bd1074c8686f043e5171
shamimWP <reza@ipvision-inc.com>
2016-05-04 18:49:53 +0600

    first

---
+++ PopUpAtBottom/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PopUpAtBottom.Resources;
+using System.Windows.Controls.Primitives;
+
+namespace PopUpAtBottom
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+
+        Popup popUP = new Popup();
+        public MainPage()
+        {
+            InitializeComponent();
+            this.SizeChanged += MainPage_SizeChanged;
+        }
+
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (popUP.IsOpen)
+                popUP.IsOpen = false;
+            popUP.Child = new PopUP();
+            popUP.VerticalOffset = e.NewSize.Height + 30 - 50;
+            popUP.IsOpen = true;
+        }        
+    }
+}
---
+++ PopUpAtBottom/PopUP.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PopUpAtBottom
+{
+    public partial class PopUP : UserControl
+    {
+        public PopUP()
+        {
+            InitializeComponent();
+            //LayoutRoot.Height = 50;
+            LayoutRoot.Width = App.Current.Host.Content.ActualWidth;
+        }
+    }
+}
